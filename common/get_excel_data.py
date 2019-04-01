0d9c75dd2e5371edfcee09f31b0c2cb040d03eb1
刘大为 <liudawei@meizu.com>
2019-04-01 17:53:11 +0800

    测试git提交

--- common/get_excel_data.py
+++ common/get_excel_data.py
@@ -8,6 +8,7 @@ def get_excel_all_data(data_file_name, sheet_name):
     #新建一个空的列表，存放所有数据
     data_list = []
 
+
     #打开excel并获取其数据
     get_execl = xlrd.open_workbook(data_file_name)
     get_sheet = get_execl.sheet_by_name(sheet_name)
@@ -15,6 +16,7 @@ def get_excel_all_data(data_file_name, sheet_name):
     #获取当前sheet页标题栏
     get_header = get_sheet.row_values(0)
 
+    #获取到当前sheet页所有数据
     for i in range(1, get_sheet.nrows):
         even_row_data = dict(zip(get_header, get_sheet.row_values(i)))
         data_list.append(even_row_data)
