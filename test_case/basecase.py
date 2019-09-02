df583df06a376bb612f5bc55e8743c7e27320196
刘大为 <liudawei@meizu.com>
2019-09-02 19:31:24 +0800

    添加获取token方法

--- test_case/basecase.py
+++ test_case/basecase.py
@@ -40,7 +40,7 @@ class BaseCase(unittest.TestCase):
             case_log_info(case_name, url, expect, res1)
         else:
             # post请求传参用data
-            res1 = requests.post(url = url, data = json.dumps(params), headers = json.loads(headers))
+            res1 = requests.post(url = url, data = json.dumps(params), headers = json.dumps(headers))
             case_log_info(case_name, url, expect, res1)
         res = [expect, res1]
         return res
@@ -71,6 +71,42 @@ class BaseCase(unittest.TestCase):
         result_expect = int(res[0])
         result = [result_expect, result_actual]
         return result
+
+
+    """获取token"""
+    def login_flyme(self):
+        url = 'https://api.meizu.com/oauth/token'
+        data = {"scope": "trust",
+                "remember_me": "mBj1R9Dg2CHmaenrXxOnanvSNfywuVZ5hHb4i9dFa2O2ZgOk88-mT_WuKgB0Peo9r6eT1hy7smsg1ORAJBMG_g6rfm8dMoMgvUagl4M4KPXWN9n46Alt8ZHREfv4cVe9UTq_2zyZuT44F8vFQ9ESpW0I3Vcc_zKYVSH-OwiSZyw*Uf2WxAO8SFhGSAwSQWPTI-XtAJO1p4C8WihXwwANTl_CmOAGjs7zRmEri9-VNo4vO0Xup-5y6bNm6n5tIsNxvmbfOZJg3WT5ohs0rNJV4GpJNcZuKELN3jbOaLmLNmvbyAVvqGus4the2XnpWWxF1ElMpLHvB42Eah1wfg0_r7A",
+                "client_id": "KzA76k3lBCYDqKTy6VYvb9WR6QSUWVGJ",
+                "client_secret": "G22l4Wj22DT9tKawFqi5x13w0TTalxxv",
+                "info": "base",
+                "grant_type": "remember_me",
+                "sn": "S25QACP56DK2M",
+                "imei": "861936030053089",
+                "device_model": "M68B0",
+                "lang": "zh_CN"}
+
+        res = requests.post(url=url, data=data, verify=False)
+        res1 = json.loads(res.text)['access_token']
+        return res1
+
+    def get_token(self):
+        url = 'https://api.meizu.com/oauth/clients/uberauth'
+        data = {"scope": "basic",
+                "client_id": "com.meizu.mzsyncservice",
+                "sn": "S25QACP56DK2M",
+                "imei": "861936030053089",
+                "device_model": "M68B0",
+                "access_token": self.login_flyme(),
+                "lang": "zh_CN"}
+
+
+        res = requests.post(url=url, data=data, verify=False)
+        res1 = json.loads(res.text)['access_token']
+        return res1
+
+
 
 
 
